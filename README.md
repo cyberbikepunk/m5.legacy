fe4c8206550871ab39bed7b5c4207c56ba69d6d9
opabinia <loic.jounot@gmail.com>
2014-12-30 20:20:47 +0100

    Second commit:  added README and LICENCE files, as well as the ".gitignore" file

---
+++ README.md
@@ -0,0 +1,14 @@
+Project-x helps you become a better bike messenger!
+
+This little program can be used by any freelancers working
+for Messenger. Just provide your username and password and
+the program will analyze the data that pertains to you
+on the company server. Have fun!
+
+!!! THIS CODE CAN BE DANGEROUS !!!
+
+As long as this program is used freely by the bike messenger
+themselves, everything is cool. As soon as it's used by the
+company management, however, all hell breaks loose. So managers
+watch out: if you ever use my code to track the performance
+of bike messengers, I will despise you and you will go to hell.
--- main.py
+++ main.py
@@ -1,38 +1,5 @@
 #!/usr/bin/python
 
-"""
-
-Project-x helps you become a better bike messenger!
-
-This little program can be used by all freelancers working
-for Messenger. Just provide your username and password and
-the program will analyze the data that pertains to you
-on the company server. Have fun!
-
-IMPORTANT NOTE! THIS CODE CAN BE DANGEROUS:
-
-As long as this program is used freely by the bike messenger
-themselves, everything is cool. As soon as it's used by the
-company management, however, all hell breaks loose. So managers
-watch out: if you ever use my code to track the performance
-of messengers, I will despise you and you will go to hell.
-
-"""
-
-# © Copyright 2014 Loic Jounot
-#
-# Project-x is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3,
-# as published by the Free Software Foundation.
-#
-# Project-x is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# The GNU/GPL v3 is found here: http://www.gnu.org/licenses/.
-
-
 from user import *
 
 
