93fc1a3c09b5c6b83ff9c55dc70b9573bbab27f4
opabinia <loic.jounot@gmail.com>
2015-01-12 18:32:28 +0100

    Part of the scraping now working. The addresses are still not implemented.

--- scrape.py
+++ scrape.py
@@ -1,13 +1,45 @@
 from bs4 import BeautifulSoup
+import re
 
-file = open('detail.html')
+file = open('19.12.2014-2974278.html')
 soup = BeautifulSoup(file)
-# soup.prettify()
+order = soup.find(id='order_detail')
+fields = dict()
 
-orders = soup.select("#order_detail")
-for order in orders:
-    header = order.find(name='h4')
-    s = 0
-    for string in header.stripped_strings:
-        s += 1
-        print('{}: {}'.format(s, string))
+# Grab the client name and number
+client_info = order.find(name='h4')
+strings = list(client_info.stripped_strings)
+match = re.match(r'Kunde:\s(.*)\s\|\s(\d{5})', strings[0])
+fields['client_name'] = match.group(1)
+fields['client_number'] = match.group(2)
+
+# Grab the job id and the payment method
+job_info = order.find(name='h2')
+strings = list(job_info.stripped_strings)
+match = re.match(r'(?:BAR\s)?(\d{10})', strings[0])
+fields['id'] = match.group(1)
+
+# Grab the the price information
+price_info = order.find(name='tbody')
+strings = list(price_info.stripped_strings)
+# rearrange a one-dimensional list of cells
+# into a dictionary
+sub_items = strings[1::2]
+sub_total = strings[::2]
+prices = dict(zip(sub_total, sub_items))
+# Substitute german with english keys
+KEYS = [('Stadtkurier', 'city_tour'),
+        ('OV.', 'overnight'),
+        ('EmpfangsbestÃ¤t.',  'same_day_fax_return'),
+        ('Wartezeit min.',  'waiting_time')]
+for old, new in KEYS:
+    if old in prices:
+        prices[new] = prices.pop(old)
+fields.update(prices)
+print(fields)
+
+# Grab all pick-ups and drop-offs
+stops_info = order.find_all(name='div', attrs={'data-collapsed': 'true'})
+for stop_info in stops_info:
+    strings = list(stop_info.stripped_strings)
+    print(strings)
