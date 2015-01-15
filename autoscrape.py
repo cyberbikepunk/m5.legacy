c0cfd4a3ae03f04fb9adbd9b2a41f298e10361c5
opabinia <loic.jounot@gmail.com>
2015-01-15 10:34:38 +0100

    This version of autoscrape.py uses two blueprints to regex out the document (one for miscellaneous sections, one for multiple addresses) but is still doesn't work for all cases. We still have a problem when regexing more than one field because fields may or may not exist.

--- autoscrape.py
+++ autoscrape.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import re
 
 
-def _blueprints():
+def _attributes_blueprints():
     """
-    Define the blueprints for the section scraper. The order of the list is
-    important. Each blueprint contains information about:
+    Define the blueprints for the attributes scraper. The order of the
+    list is important. Each blueprint contains information about:
         - which tag to target in the DOM
         - which lines to scrape inside the tag
         - what regex pattern to run on each line
@@ -13,119 +13,182 @@ def _blueprints():
 
     :return: (list) A list of blueprint dictionaries
     """
-    # Three sections appear strictly once
-    blueprints = list(range(3))
+    # Define three blueprints
+    attributes_blueprints = list(range(3))
 
-    # General information
-    blueprints[0] = {
+    # General attributes
+    attributes_blueprints[0] = {
         'tag': 'h2',
         'lines': [0],
         'patterns': [r'(BAR\s)?(\d{10})'],
         'fields': [['cash_payment', 'id']]
     }
-    # Client information
-    blueprints[1] = {
+    # Client attributes
+    attributes_blueprints[1] = {
         'tag': 'h4',
         'lines': [0],
         'patterns': [r'Kunde:\s(.*)\s\|\s(\d{5})'],
         'fields': [['client_name', 'client_number']]
     }
-    # Trip information
-    blueprints[2] = {
+    # Trip attributes
+    attributes_blueprints[2] = {
         'tag': 'p',
         'lines': [0],
         'patterns': [r'(\d{1,2},\d{3})\skm'],
         'fields': [['km']]
     }
+    return attributes_blueprints
 
-    # Arbitrary number of stops
-
-
-    return blueprints
-
-def _scrape_section(bp, source):
+def _scrape_attributes(blueprint, soup_subset):
     """
-    Scrape off a small section contained inside a tag
-    with BeautifulSoup and a little regex.
+    Scrape off the contents contained inside a given html tag
+    using the blueprint we've prepared.
 
-    :param bp: (dict) The blueprint for the section
-    :param source: (str) cleaned up html produced by BeautifulSoup
+    :param blueprint: (dict) The blueprint for the tag contents
+    :param soup_subset: (tag object) cleaned up html
     :return: (dict) field name/value pairs
     """
-    contents = list(source.stripped_strings)
+    contents = list(soup_subset.stripped_strings)
     fields = dict()
 
-    for line in bp['lines']:
-        match = re.match(bp['patterns'][line], contents[line])
-        for index, name in enumerate(bp['fields'][line]):
+    for line in blueprint['lines']:
+        match = re.match(blueprint['patterns'][line], contents[line])
+        for index, name in enumerate(blueprint['fields'][line]):
             # Indices for matched groups start at 1
             fields[name] = match.group(index+1)
 
     return fields
 
-
-def scrape_prices(source):
+def _scrape_prices(soup_subset):
     """
-    Scrape the 'prices' table at the bottom of the page. This section is
+    Scrape off the 'prices' table at the bottom of the page. This section is
     scraped seperately precisely because it's already formatted as a table.
 
-    :param source: (str) cleaned up html produced by BeautifulSoup
+    :param soup_subset: (tag object) cleaned up html
     :return: (dict) field name/value pairs
     """
-    # Grab the only html table in the document
-    html_subset = source.find(name='tbody')
-    cells = list(html_subset.stripped_strings)
-
-    # The table is returned as a one-dimensional list
+    # The table is grabbed as a one-dimensional list
     # of cells but we want it in dictionary format.
-    prices = dict(zip(cells[::2], cells[1::2]))
+    cells = list(soup_subset.stripped_strings)
+    prices_table = dict(zip(cells[::2], cells[1::2]))
 
     # Original field names are no good. Change them.
-    keys = [('Stadtkurier', 'city_tour'),
-            ('Stadt Stopp(s)', 'extra_stops'),
-            ('OV.', 'overnight'),
-            ('EmpfangsbestÃ¤t.',  'fax_return'),
-            ('Wartezeit min.',  'waiting_time')]
+    keys = [
+        ('Stadtkurier',         'city_tour'),
+        ('Stadt Stopp(s)',      'extra_stops'),
+        ('OV.',                 'overnight'),
+        ('EmpfangsbestÃ¤t.',    'fax_confirm'),
+        ('Wartezeit min.',      'waiting_time')
+    ]
     for old, new in keys:
-        if old in prices:
-            prices[new] = prices.pop(old)
+        if old in prices_table:
+            prices_table[new] = prices_table.pop(old)
 
-    return prices
+    return prices_table
 
-def main(filename):
-    file = open(filename)
-    html_source = BeautifulSoup(file).find(id='order_detail')
+def _address_blueprint():
+    """
+    Define the blueprint to scrape a single address.
+
+    :return: (dict) A blueprint to scrape addresses
+    """
+    address_blueprint = {
+        'lines': [0, 1, 2, 3, 6, 7],
+        'patterns': [
+            r'(Abholung)|(Zustellung)',
+            r'(.*)',
+            r'(.*)',
+            r'(\d{5})\s(.*)',
+            r'(?:.*)ab\s(\d{2}:\d{2})*(?:.*)bis\s(\d{2}:\d{2})*',
+            r'ST:\s(\d{2}:\d{2})'
+        ],
+        'fields': [
+            ['purpose'],
+            ['company'],
+            ['address'],
+            ['postal_code', 'city'],
+            ['from', 'until'],
+            ['timestamp']
+        ]
+    }
+    return address_blueprint
+
+def _scrape_address(soup_subset):
+    """
+    Scrape off all fields associated with an address, including for example
+    the pick-up or drop-off time window.
+
+    :param soup_subset: (str) cleaned up html
+    :return: (dict) field name/value pairs
+    """
+    blueprint = _address_blueprint()
+    contents = list(soup_subset.stripped_strings)
     fields = dict()
 
-    # Scrape the prices at the bottom of the page
-    prices = scrape_prices(html_source)
-    fields.update(prices)
+    for line_index, line in enumerate(blueprint['lines']):
+        print(blueprint['patterns'][line_index])
+        match = re.match(blueprint['patterns'][line_index], contents[line])
+        for field_index, name in enumerate(blueprint['fields'][line_index]):
+            # Indices for matched groups start at 1
+            fields[name] = match.group(field_index+1)
 
-    # Scrape multiple subsections of the page
-    # using the blueprints we've prepared
-    blueprints = _blueprints()
-    for blueprint in blueprints:
-        html_subset = html_source.find_next(blueprint['tag'])
-        fields_subset = _scrape_section(blueprint, html_subset)
-        fields.update(fields_subset)
+    return fields
 
-    print(filename)
-    print(fields)
+def scrape_job(html):
+    """
+    Scrape the shit out of the html document using BeautifulSoup
+    and a little regex, in three steps: first, job attributes,
+    then prices and finally addresses. Attributes and prices
+    are returned as dictionaries and addresses as a list of
+    dictionaries. Each dictionary contains field name/value
+    pairs. All values are raw strings.
 
+    :param html: (tag object) cleaned up html produced by BeautifulSoup
+    :return: (tuple) attributes, prices, addresses
+    """
+    # Scrape various job attributes in various places
+    # attributes = dict()
+    # for blueprint in _attributes_blueprints():
+    #     html_subset = html.find_next(blueprint['tag'])
+    #     fields = _scrape_attributes(blueprint, html_subset)
+    #     attributes.update(fields)
 
-filenames = [
-    '19.12.2014-2973926.html',
-    '19.12.2014-2974276.html',
-    '19.12.2014-2974278.html',
-    '19.12.2014-2974685.html',
-    '19.12.2014-2974730.html',
-    '19.12.2014-2974918.html',
-    '19.12.2014-2975095.html',
-    '19.12.2014-2975147.html',
-    '19.12.2014-2976587.html',
-    '19.12.2014-2976667.html',
-    '19.12.2014-2976786.html',
-    '19.12.2014-2977728.html',
+    # Scrape the prices from the only table on the page
+    # html_subset = html.find(name='tbody')
+    # prices = _scrape_prices(html_subset)
+
+    # Scrape an arbitrary number of addresses. A normal city tour has
+    # usually two addresses but sometimes more. An overnight tour has
+    # strictly two addresses, but the second is a delivery point outside
+    # of Berlin (and therefore irrelevant). We deal with these issues
+    # downstream.
+    addresses = list()
+    html_subsets = html.find_all(name='div', attrs={'data-collapsed': 'true'})
+    for html_subset in html_subsets:
+        address = _scrape_address(html_subset)
+        addresses.append(address)
+
+    return attributes, prices, addresses
+
+def main():
+    filenames = [
+        '19.12.2014-2973926.html',
+        '19.12.2014-2974276.html',
+        '19.12.2014-2974278.html',
+        '19.12.2014-2974685.html',
+        '19.12.2014-2974730.html',
+        '19.12.2014-2974918.html',
+        '19.12.2014-2975095.html',
+        '19.12.2014-2975147.html',
+        '19.12.2014-2976587.html',
+        '19.12.2014-2976667.html',
+        '19.12.2014-2976786.html',
+        '19.12.2014-2977728.html',
     ]
-for f in filenames:
-    main(f)
+    for f in filenames:
+        file_handle = open(f)
+        soup = BeautifulSoup(file_handle).find(id='order_detail')
+        data = scrape_job(soup)
+        print(data)
+
+main()
